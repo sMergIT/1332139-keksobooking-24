191e697fbdc92e62b594e91188662cd95d5b9d31
Keks <keks@htmlacademy.ru>
2021-10-01 14:07:53 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,10 +1,15 @@
-function randomInteger(min,max) {
+function randomInteger(min, max) {
   // случайное число от min до (max+1)
   const number = min + Math.random() * (max + 1 - min);
   return Math.floor(number);
 }
-alert (randomInteger(0 , 99));
+alert(randomInteger(0, 99));
 
-const decimal = 12345.789;
-decimal.toFixed ( 3 );
-alert (decimal);
+
+function randomFractional(min, max) {
+  // случайное число от min до (max+1)
+  const decimal =  min + Math.random() * (max + 1 - min);
+  return decimal.toFixed(3);
+  //убирает знаки после запятой
+}
+alert(randomFractional(0, 3.5))
