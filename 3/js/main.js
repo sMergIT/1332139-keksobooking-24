5872e66594ea2041131f43a06740925bd558c287
Keks <keks@htmlacademy.ru>
2021-11-02 17:51:43 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,9 +1,14 @@
+const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const checkIns = ['12:00', '13:00', '14:00'];
+const checkOuts = ['12:00', '13:00', '14:00'];
+const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
 function getRandomInteger(min, max) {
   // случайное число от min до (max+1)
   const number = min + Math.random() * (max + 1 - min);
   return Math.floor(number);
 }
-console.log(getRandomInteger(0, 99));
 
 function getRandomFractional(min, max, rate) {
   // случайное число от min до (max+1)
@@ -11,16 +16,6 @@ function getRandomFractional(min, max, rate) {
   return decimal.toFixed(rate);
   //убирает знаки после запятой
 }
-console.log(getRandomFractional(0, 3.555, 9));
-
-const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const checkIns = ['12:00', '13:00', '14:00'];
-const checkOuts = ['12:00', '13:00', '14:00'];
-const lat = getRandomFractional(35.65000, 35.70000, 5);
-const lng = getRandomFractional(139.70000, 139.80000, 5);
-const offers = generateOffers();
-const feature = generateFeatures();
-const photo = generatePhoto();
 
 function generateOffers() {
   const offers = [];
@@ -29,6 +24,8 @@ function generateOffers() {
     const index = getRandomInteger(0, types.length - 1);
     const indexCheckin = getRandomInteger(0, checkIns.length - 1);
     const indexCheckout = getRandomInteger(0, checkOuts.length - 1);
+    const lat = getRandomFractional(35.65000, 35.70000, 5);
+    const lng = getRandomFractional(139.70000, 139.80000, 5);
 
     offers[i] = {
       author: {
@@ -50,16 +47,14 @@ function generateOffers() {
       location: {
         lat: lat,
         lng: lng,
-      }
+      },
     };
-
   }
   return offers;
-};
+}
 
 function generateFeatures() {
   const conveniences = [];
-  const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
   const maxLength = features.length;
   const lengthOfArray = getRandomInteger(1, maxLength);
   while (conveniences.length < lengthOfArray) {
@@ -70,23 +65,18 @@ function generateFeatures() {
     }
   }
   return conveniences;
-};
+}
 
 function generatePhoto() {
-  const pixs = [];
-  const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+  const pictures = [];
   const maxLengthPhoto = photos.length;
   const lengthOfArrays = getRandomInteger(1, maxLengthPhoto);
-  while (pixs.length < lengthOfArrays) {
+  while (pictures.length < lengthOfArrays) {
     const indexOfPhotos = getRandomInteger(0, maxLengthPhoto - 1);
     const indexPhotos = photos[indexOfPhotos];
-    if (!pixs.includes(indexPhotos)) {
-      pixs.push(indexPhotos);
+    if (!pictures.includes(indexPhotos)) {
+      pictures.push(indexPhotos);
     }
   }
-  return pixs;
-};
-
-console.log(offers);
-console.log(feature);
-console.log(photo);
+  return pictures;
+}
