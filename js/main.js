0ac4ba6556796573614cb690978431be79fd66e0
ilya <nivaiz54@gmail.com>
2021-11-06 02:22:24 +0500

    Попытка выполения дз

--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@ import './util.js';
 import './data.js';
 import './backend.js';
 import './card.js';
+import './map.js';
+import './utils/debounce.js';
+import './utils/throttle.js';
--- js/util.js
+++ js/util.js
@@ -3,12 +3,10 @@ function getRandomInteger(min, max) {
   const number = min + Math.random() * (max + 1 - min);
   return Math.floor(number);
 }
-
 function getRandomFractional(min, max, rate) {
   // случайное число от min до (max+1)
   const decimal = min + Math.random() * (max + 1 - min);
   return decimal.toFixed(rate);
   //убирает знаки после запятой
 }
-
-export {getRandomInteger,getRandomFractional};
+export { getRandomInteger, getRandomFractional };
