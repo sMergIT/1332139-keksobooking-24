3c279c89720005d6d614d53fdb83fd6299c7b039
Keks <keks@htmlacademy.ru>
2021-11-16 12:52:03 +0300

    :heavy_check_mark: Сборка #15

---
+++ 15/js/popup-form.js
@@ -0,0 +1,31 @@
+const adForm = document.querySelector('.ad-form');
+const adFormElement = adForm.querySelectorAll('.ad-form__element');
+const mapFilters = document.querySelector('.map__filters');
+const mapfiltersElementAll = mapFilters.querySelectorAll('.map__filter');
+const mapFiltresElementFeatures = mapFilters.querySelector('.map__features');
+
+const activePopupForm = () => {
+  adForm.classList.remove('ad-form--disabled');
+  mapFilters.classList.remove('map__filters--disabled');
+  adFormElement.forEach((adFormElements) => {
+    adFormElements.removeAttribute('disabled', '');
+  });
+  mapfiltersElementAll.forEach((mapFiltersElementAlls) => {
+    mapFiltersElementAlls.removeAttribute('disabled', '');
+  });
+  mapFiltresElementFeatures.removeAttribute('disabled', '');
+};
+
+const deactivatePopupForm = () => {
+  adForm.classList.add('ad-form--disabled');
+  mapFilters.classList.add('map__filters--disabled');
+  adFormElement.forEach((adFormElements) => {
+    adFormElements.setAttribute('disabled', '');
+  });
+  mapfiltersElementAll.forEach((mapfiltersElementAlls) => {
+    mapfiltersElementAlls.setAttribute('disabled', '');
+  });
+  mapFiltresElementFeatures.setAttribute('disabled', '');
+};
+
+export { activePopupForm, deactivatePopupForm };
