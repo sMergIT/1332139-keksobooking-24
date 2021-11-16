eec5f400f864c8aff108da5f0f6554eab5b35ab3
ilya <nivaiz54@gmail.com>
2021-11-16 14:53:49 +0500

    Попробовал исправить ошибки123

--- js/map.js
+++ js/map.js
@@ -18,7 +18,7 @@ L.tileLayer(
 ).addTo(map);
 
 const mainPinIcon = L.icon({
-  iconUrl: '../img/main-pin.svg',
+  iconUrl: '.../img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
@@ -39,7 +39,7 @@ mainPin.addTo(map);
 const setPins = () => {
   announcements.map((card) => {
     const icon = L.icon({
-      iconUrl: '../img/pin.svg',
+      iconUrl: '.../img/pin.svg',
       iconSize: [40, 40],
       iconAnchor: [20, 40],
     });
