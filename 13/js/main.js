d67cb437fd7e9f967367725e04111792641acb30
Keks <keks@htmlacademy.ru>
2021-11-16 12:23:41 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/main.js
@@ -0,0 +1,7 @@
+import './util.js';
+import './data.js';
+import './backend.js';
+import './card.js';
+import './map.js';
+import './form.js';
+import './popup-form.js';
---
+++ 13/js/util.js
@@ -0,0 +1,11 @@
+export function getRandomInteger(min, max) {
+  // случайное число от min до (max+1)
+  const number = min + Math.random() * (max + 1 - min);
+  return Math.floor(number);
+}
+export function getRandomFractional(min, max, rate) {
+  // случайное число от min до (max+1)
+  const decimal = min + Math.random() * (max + 1 - min);
+  return decimal.toFixed(rate);
+  //убирает знаки после запятой
+}
