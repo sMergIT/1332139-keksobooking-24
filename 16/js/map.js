6b2970ff82b3221f5ce3fd558ab2ddbc414deaea
Keks <keks@htmlacademy.ru>
2021-11-17 16:57:41 +0300

    :heavy_check_mark: Сборка #16

--- 16/js/map.js
+++ 16/js/map.js
@@ -21,7 +21,7 @@ L.tileLayer(
 ).addTo(map);
 
 const mainPinIcon = L.icon({
-  iconUrl: '../img/main-pin.svg',
+  iconUrl: 'img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
@@ -41,7 +41,7 @@ mainPin.addTo(map);
 
 announcements.map((card) => {
   const icon = L.icon({
-    iconUrl: '../img/pin.svg',
+    iconUrl: 'img/pin.svg',
     iconSize: [40, 40],
     iconAnchor: [20, 40],
   });
