e2a9ec5b8ae84f03c56bd5bf36b1aa37c06f4a78
Keks <keks@htmlacademy.ru>
2021-11-17 20:58:35 +0300

    :heavy_check_mark: Сборка #17

---
+++ 17/js/main.js
@@ -0,0 +1,8 @@
+import './util.js';
+import './data.js';
+import './backend.js';
+import './card.js';
+import './map.js';
+import './form.js';
+import './popup-form.js';
+import './popupuser.js';
---
+++ 17/js/map.js
@@ -0,0 +1,80 @@
+import { getMarkupSimilarAnnouncements } from './card.js';
+import { announcements } from './data.js';
+import { adClearForm } from './form.js';
+import { activatePopupForm } from './popup-form.js';
+
+const addressInput = document.querySelector('#address');
+const map = L.map('map-canvas').on('load', () => {
+  addressInput.value = '35.681729, 139.753927';
+  activatePopupForm();
+})
+  .setView({
+    lat: 35.681729,
+    lng: 139.753927,
+  }, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: 'img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const mainPin = L.marker(
+  {
+    lat: 35.6895,
+    lng: 139.692,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  },
+);
+
+mainPin.addTo(map);
+
+announcements.map((card) => {
+  const icon = L.icon({
+    iconUrl: 'img/pin.svg',
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+  const marker = L.marker({
+    lat: card.location.lat,
+    lng: card.location.lng,
+  },
+  {
+    icon,
+  });
+  marker
+    .addTo(map)
+    .bindPopup(getMarkupSimilarAnnouncements(card));
+});
+
+const resetMapAndMarker = () => {
+  mainPin.setLatLng({
+    lat: 35.6895,
+    lng: 139.692,
+  });
+
+  map.setView({
+    lat: 35.6895,
+    lng: 139.692,
+  }, 10);
+  map.closePopup();
+};
+
+adClearForm.addEventListener('click', resetMapAndMarker);
+
+mainPin.on('move', (evt) => {
+  const mainPinCoordinates = evt.target.getLatLng();
+  addressInput.value = `${mainPinCoordinates.lat.toFixed(5)}, ${mainPinCoordinates.lng.toFixed(5)}`;
+});
+
+export { resetMapAndMarker };
